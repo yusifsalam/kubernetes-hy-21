f4660a2a59eda4d61bf816e14ad24b3276f3f526
Yusif Salam-zade <yusalam@gmail.com>
2021-11-14 22:27:00 +0200

    exercise 1.02: Project v0.1
    Simple fastify server that listens on port 3000

---
+++ project/src/index.ts
@@ -0,0 +1,18 @@
+import fastify, { FastifyInstance } from "fastify";
+
+const server: FastifyInstance = fastify({});
+
+const start = async () => {
+  try {
+    await server.listen(3000);
+
+    const address = server.server.address();
+    const port = typeof address === "string" ? address : address?.port;
+    console.log(`Server started on port ${port}`);
+  } catch (err) {
+    server.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
