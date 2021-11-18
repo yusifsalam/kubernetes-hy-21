ce99a211ca353121079b52c448e41742298654f7
Yusif Salam-zade <yusalam@gmail.com>
2021-11-18 17:47:36 +0200

    exercise 1.05: Project v0.3

--- project/src/index.ts
+++ project/src/index.ts
@@ -1,10 +1,23 @@
 import fastify, { FastifyInstance } from "fastify";
+import fastifyStatic, { FastifyStaticOptions } from "fastify-static";
+import path from "path";
 
-const server: FastifyInstance = fastify({});
+const server: FastifyInstance = fastify({ logger: true });
+
+const fastifyStaticOptions: FastifyStaticOptions = {
+  root: path.join(process.cwd(), "public"),
+  prefix: "/public/",
+};
+
+server.register(fastifyStatic, fastifyStaticOptions);
+
+server.get("/", async (_req, res) => {
+  return res.sendFile("hello.html");
+});
 
 const start = async () => {
   try {
-    await server.listen(3000);
+    await server.listen(3000, "0.0.0.0");
 
     const address = server.server.address();
     const port = typeof address === "string" ? address : address?.port;
