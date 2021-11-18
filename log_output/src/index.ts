840d87581df0558aa5cc79e4d0ba8a241141f157
Yusif Salam-zade <yusalam@gmail.com>
2021-11-18 18:26:21 +0200

    exercise 1.07: External access with Ingress

--- log_output/src/index.ts
+++ log_output/src/index.ts
@@ -1,8 +1,25 @@
-import { v4 as uuid } from "uuid";
+import fastify, { FastifyInstance } from "fastify";
+import { getHash } from "./utils/getHash";
 
-const getHash = () => {
-  console.log(new Date().toISOString() + " " + uuid());
-  setTimeout(getHash, 5000);
+const server = fastify({ logger: true });
+
+server.get("/", async (_req, res) => {
+  const hash = getHash();
+  console.log(hash);
+  res.send(hash);
+});
+
+const start = async () => {
+  try {
+    await server.listen(3000, "0.0.0.0");
+
+    const address = server.server.address();
+    const port = typeof address === "string" ? address : address?.port;
+    console.log(`Server started on port ${port}`);
+  } catch (err) {
+    server.log.error(err);
+    process.exit(1);
+  }
 };
 
-getHash();
+start();
