7912d8af1e025aa0bfb3c9098790bf6b6a4ad585
Yusif Salam-zade <yusalam@gmail.com>
2021-11-21 23:13:59 +0200

    exercise 2.02: Project v1.0

--- project/src/routes/root.ts
+++
@@ -1,9 +0,0 @@
-import { FastifyPluginAsync } from "fastify";
-
-const rootRoute: FastifyPluginAsync = async (fastify): Promise<void> => {
-  fastify.get("/", async (_req, res) => {
-    res.sendFile("hello.html");
-  });
-};
-
-export default rootRoute;
