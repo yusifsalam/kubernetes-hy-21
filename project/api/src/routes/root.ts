f45a60d3bb2f81cab4107f2690dd7d6eb0b42b92
Yusif Salam-zade <yusalam@gmail.com>
2021-12-01 20:24:14 +0200

    add / path to solve GKE health probe

--- project/api/src/routes/root.ts
+++ project/api/src/routes/root.ts
@@ -4,6 +4,10 @@ const rootRoute: FastifyPluginAsync = async (fastify): Promise<void> => {
   fastify.get("/api", async (_req, res) => {
     res.sendFile("hello.html");
   });
+
+  fastify.get("/", async (_req, res) => {
+    res.send({ status: "ok" });
+  });
 };
 
 export default rootRoute;
