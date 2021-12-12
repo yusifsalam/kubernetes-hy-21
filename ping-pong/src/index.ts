52bb8354775e384634b9eff5e673029bb4d7b886
Yusif Salam-zade <yusalam@gmail.com>
2021-12-12 17:33:31 +0200

    exercise 5.05: Deploy to Serverless

--- ping-pong/src/index.ts
+++ ping-pong/src/index.ts
@@ -9,6 +9,8 @@ let pongs: number = 0;
 let dbSuccess = false;
 let dbConnectAttemps = 0;
 
+const PORT = Number(process.env.MY_PORT) || 8080;
+
 server.get("/pingpong", async (_req, res) => {
   pgClient.query(`update pings set pongs = ${++pongs} where id = 1;`);
   res.send(`pong ${pongs}`);
@@ -57,7 +59,7 @@ const connectToDb = async () => {
 const start = async () => {
   try {
     await connectToDb();
-    await server.listen(3000, "0.0.0.0");
+    await server.listen(PORT, "0.0.0.0");
     const address = server.server.address();
     const port = typeof address === "string" ? address : address?.port;
     console.log(`Server started on port ${port}`);
